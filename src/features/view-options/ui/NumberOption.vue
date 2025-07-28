133a8c0eb8309a276a62798cf03564025173219b
solant <runner62v6@gmail.com>
2025-07-29 01:38:05 +0200

    feat: update number options design

--- src/features/view-options/ui/NumberOption.vue
+++ src/features/view-options/ui/NumberOption.vue
@@ -8,6 +8,14 @@ const model = defineModel<number>({ required: true });
 
 const props = defineProps<{ title: string; step?: number; fixed?: number }>();
 
+function increment() {
+  model.value += props.step ?? 1;
+}
+
+function decrement() {
+  model.value -= props.step ?? 1;
+}
+
 const scrollEnabled = ref(false);
 useEventListener(
   'wheel',
@@ -17,9 +25,9 @@ useEventListener(
     }
 
     if (e.deltaY < 0) {
-      model.value += props.step ?? 1;
+      increment();
     } else {
-      model.value -= props.step ?? 1;
+      decrement();
     }
   },
   { passive: false },
@@ -28,15 +36,32 @@ useEventListener(
 
 <template>
   <OptionItem :title>
-    <input
-      type="number"
-      :value="model.toFixed(props.fixed ?? 0)"
-      @update="
-        (event: InputEvent) => (model = Number.parseFloat((event.target as HTMLInputElement).value))
-      "
-      :step
+    <div
+      class="flex items-stretch border-1 border-solid border-gray-300 rounded"
       @mouseenter="scrollEnabled = true"
       @mouseleave="scrollEnabled = false"
-    />
+    >
+      <button
+        @click="decrement"
+        class="border-r border-solid border-gray-300 w-[24px] flex items-center justify-center"
+      >
+        <span class="i-mdi:minus block" />
+      </button>
+      <input
+        class="max-w-[70px] appearance-none text-center focus:outline-none"
+        :value="model.toFixed(props.fixed ?? 0)"
+        @update="
+          (event: InputEvent) =>
+            (model = Number.parseFloat((event.target as HTMLInputElement).value))
+        "
+        :step
+      />
+      <button
+        @click="increment"
+        class="border-l border-solid border-gray-300 w-[24px] flex items-center justify-center"
+      >
+        <span class="i-mdi:plus block" />
+      </button>
+    </div>
   </OptionItem>
 </template>
